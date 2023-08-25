f72f9330ec17519e682a601a32f6d230c5e2552e
pubrrr <pubr.maeae@web.de>
2023-08-25 23:49:30 +0200

    make Vercel deployment work

--- vercel-build.sh
+++ vercel-build.sh
@@ -7,5 +7,12 @@ source "$HOME/.cargo/env"
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 
+echo "Building:"
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-name dndhelper_wasm --out-dir wasm --target web target\wasm32-unknown-unknown\release\dndhelper.wasm
+
+echo "Generating WASM JS:"
+wasm-bindgen --out-name dndhelper_wasm --out-dir ./public/wasm --target web target/wasm32-unknown-unknown/release/dndhelper.wasm
+
+echo "done"
+ls -al public
+ls -al public/wasm
