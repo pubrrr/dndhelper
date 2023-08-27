b7af64637872b4695da08baea888a52e6cf9e9f3
pubrrr <pubr.maeae@web.de>
2023-08-27 15:45:12 +0200

    Copy assets for Vercel deployment

--- vercel-build.sh
+++ vercel-build.sh
@@ -12,4 +12,7 @@ cargo build --release --target wasm32-unknown-unknown
 echo "Generating WASM JS:"
 wasm-bindgen --out-name dndhelper_wasm --out-dir ./public/wasm --target web target/wasm32-unknown-unknown/release/dndhelper.wasm
 
+echo "Copying assets:"
+cp -r ./assets ./public/assets
+
 echo "done"
