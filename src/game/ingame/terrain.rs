2e621801ac6573541212e099936cf9dc98fd8d3f
pubrrr <pubr.maeae@web.de>
2024-04-13 14:42:08 +0200

    refactor

--- src/game/ingame/terrain.rs
+++ src/game/ingame/terrain.rs
@@ -24,10 +24,37 @@ impl Display for MovementCost {
 }
 
 impl MovementCost {
+    /// `field_of_movement` adds 1 to all costs, so we need to subtract 1 from the cost
     pub(super) fn get_modified_algorithm_cost(&self) -> Option<u32> {
         match self {
             MovementCost::Impassable => None,
             MovementCost::Passable(cost) => Some(max(*cost as u32, 1) - 1),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_modified_algorithm_cost() {
+        assert_eq!(MovementCost::Impassable.get_modified_algorithm_cost(), None);
+        assert_eq!(
+            MovementCost::Passable(0).get_modified_algorithm_cost(),
+            Some(0)
+        );
+        assert_eq!(
+            MovementCost::Passable(1).get_modified_algorithm_cost(),
+            Some(0)
+        );
+        assert_eq!(
+            MovementCost::Passable(2).get_modified_algorithm_cost(),
+            Some(1)
+        );
+        assert_eq!(
+            MovementCost::Passable(3).get_modified_algorithm_cost(),
+            Some(2)
+        );
     }
 }
