39bffd8a68b4ca28c82d1fb38acc27076c6dd379
pubrrr <pubr.maeae@web.de>
2024-04-18 22:36:53 +0200

    disentangle: put it in a one shot system

--- src/game/abilities/active_abilities.rs
+++ src/game/abilities/active_abilities.rs
@@ -1,15 +1,20 @@
-use bevy::log::warn;
 use std::collections::HashSet;
 
-use bevy::prelude::{Component, FromWorld, Parent, Query, With, World};
+use bevy::ecs::system::SystemId;
+use bevy::log::warn;
+use bevy::prelude::{Component, Entity, EventWriter, FromWorld, In, Parent, Query, With, World};
 use bevy::utils::HashMap;
 use enum_iterator::{all, Sequence};
 use hexx::Hex;
 
+use crate::game::ingame::combat::{Attack, AttackOrDefault, CombatEvent};
+use crate::game::ingame::game_log::LogEvent;
 use crate::game::ingame::hex::{HexComponent, HexMarker};
 use crate::game::ingame::selected_unit::UpdateReachableHexesUnitsQuery;
 use crate::game::ingame::terrain::Terrain;
+use crate::game::ingame::unit::UnitFilter;
 use crate::game::util::find_units_within_range::FindUnitsWithinRange;
+
 #[derive(Component, Debug, Clone)]
 pub struct ActivatedAbilityMarker;
 
@@ -19,9 +24,11 @@ pub enum ActiveAbilityType {
 }
 
 impl ActiveAbilityType {
-    fn get_ability(&self) -> ActiveAbility {
+    fn get_ability(&self, world: &mut World) -> ActiveAbility {
         match self {
-            ActiveAbilityType::ThrowJavelin => ActiveAbility::ThrowJavelin,
+            ActiveAbilityType::ThrowJavelin => ActiveAbility::ThrowJavelin {
+                throw_javelin_system: world.register_system(throw_javelin_system),
+            },
         }
     }
 }
@@ -35,10 +42,10 @@ impl ActiveAbilityRegistry {
 }
 
 impl FromWorld for ActiveAbilityRegistry {
-    fn from_world(_: &mut World) -> Self {
+    fn from_world(world: &mut World) -> Self {
         let abilities = all::<ActiveAbilityType>()
             .map(|ability_type| {
-                let ability = ability_type.get_ability();
+                let ability = ability_type.get_ability(world);
                 (ability_type, ability)
             })
             .collect();
@@ -49,13 +56,15 @@ impl FromWorld for ActiveAbilityRegistry {
 
 #[derive(Component, Debug, Clone)]
 pub enum ActiveAbility {
-    ThrowJavelin,
+    ThrowJavelin {
+        throw_javelin_system: SystemId<ThrowJavelinInput>,
+    },
 }
 
 impl ActiveAbility {
     pub fn get_display_name(&self) -> String {
         match self {
-            ActiveAbility::ThrowJavelin => "Throw Javelin".to_string(),
+            ActiveAbility::ThrowJavelin { .. } => "Throw Javelin".to_string(),
         }
     }
 
@@ -66,7 +75,7 @@ impl ActiveAbility {
         parent: &Parent,
     ) -> Option<HashSet<Hex>> {
         match self {
-            ActiveAbility::ThrowJavelin => {
+            ActiveAbility::ThrowJavelin { .. } => {
                 let Ok((_, selected_unit_hex, selected_unit_team, _, _)) = units.get(**parent)
                 else {
                     warn!("Units query did not contain parent of activated ability {parent:?}");
@@ -81,4 +90,32 @@ impl ActiveAbility {
             }
         }
     }
+}
+
+pub struct ThrowJavelinInput {
+    pub attacker: Entity,
+    pub target_hex: Hex,
+}
+
+fn throw_javelin_system(
+    input: In<ThrowJavelinInput>,
+    units: Query<(Entity, &HexComponent), UnitFilter>,
+    mut combat_event: EventWriter<CombatEvent>,
+    mut log_event: EventWriter<LogEvent>,
+) {
+    let Some((defender, _)) = units.iter().find(|(_, hex)| hex.0 == input.target_hex) else {
+        return;
+    };
+    log_event.send(LogEvent {
+        message: format!("{:?} throws a javelin at {defender:?}", input.attacker),
+    });
+    combat_event.send(CombatEvent {
+        attack: AttackOrDefault::Attack(Attack {
+            damage: 1,
+            passive_combat_abilities: vec![],
+            range: 2,
+        }),
+        attacker: input.attacker,
+        defender,
+    });
 }
--- src/game/ingame/active_abilities_systems.rs
+++ src/game/ingame/active_abilities_systems.rs
@@ -1,28 +1,22 @@
 use bevy::input::ButtonInput;
 use bevy::prelude::{
-    info_once, warn, Commands, Entity, EventWriter, MouseButton, NextState, Parent, Query, Res,
-    ResMut, With,
+    info_once, warn, Commands, Entity, MouseButton, NextState, Parent, Query, Res, ResMut, With,
 };
 
-use crate::game::abilities::active_abilities::{ActivatedAbilityMarker, ActiveAbility};
-use crate::game::ingame::combat::{Attack, AttackOrDefault, CombatEvent};
-use crate::game::ingame::game_log::LogEvent;
-use crate::game::ingame::hex::HexComponent;
+use crate::game::abilities::active_abilities::{
+    ActivatedAbilityMarker, ActiveAbility, ThrowJavelinInput,
+};
 use crate::game::ingame::hovered_hex::HoveredHex;
 use crate::game::ingame::selected_unit::SelectedUnitResource;
-use crate::game::ingame::unit::UnitFilter;
 use crate::game::states::round_state::RoundState;
 
-#[allow(clippy::too_many_arguments)]
 pub fn handle_activated_active_ability(
     selected_unit_resource: Res<SelectedUnitResource>,
     buttons: Res<ButtonInput<MouseButton>>,
     hovered_hex: Res<HoveredHex>,
     mut round_state: ResMut<NextState<RoundState>>,
     active_abilities: Query<(&ActiveAbility, &Parent), With<ActivatedAbilityMarker>>,
-    mut combat_event: EventWriter<CombatEvent>,
-    units: Query<(Entity, &HexComponent), UnitFilter>,
-    mut log_event: EventWriter<LogEvent>,
+    mut commands: Commands,
 ) {
     let Ok((ability, parent)) = active_abilities.get_single() else {
         round_state.set(RoundState::Input);
@@ -48,24 +42,15 @@ pub fn handle_activated_active_ability(
     }
 
     match ability {
-        ActiveAbility::ThrowJavelin => {
-            let Some((defender, _)) = units.iter().find(|(_, hex)| hex.0 == hex_cursor_position)
-            else {
-                return;
-            };
-            log_event.send(LogEvent {
-                message: format!("{:?} throws a javelin at {defender:?}", **parent),
-            });
-            combat_event.send(CombatEvent {
-                attack: AttackOrDefault::Attack(Attack {
-                    damage: 1,
-                    passive_combat_abilities: vec![],
-                    range: 2,
-                }),
+        ActiveAbility::ThrowJavelin {
+            throw_javelin_system: system_id,
+        } => commands.run_system_with_input(
+            *system_id,
+            ThrowJavelinInput {
                 attacker: **parent,
-                defender,
-            })
-        }
+                target_hex: hex_cursor_position,
+            },
+        ),
     };
 }
 
